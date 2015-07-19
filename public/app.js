01ad7c23c92ee6c16751866eb13b815f2d4b352a
kartikey <kartikeybharti@gmail.com>
2015-07-18 17:57:21 -0700

    initial commit

---
+++ public/app.js
@@ -0,0 +1,49 @@
+var todoapp = angular.module('todoapp',['ngRoute']);
+
+todoapp.config(function($routeProvider) { 
+    $routeProvider
+      .when('/',
+      {
+        templateUrl: "views/main.html",
+        controller: "mainController"
+      })
+      .otherwise({
+        redirectTo : '/'
+      });
+});
+
+todoapp.controller('mainController',function($scope) {
+    $scope.formData ={};
+    
+    $http.get('/api/todos')
+        .success(function(data) {
+            $scope.todos = data;
+            console.log(data);
+    })
+        .error(function(data) {
+            console.log("Error: "+ data); 
+    });
+    
+    $scope.createTodo = function() {
+        $http.post('/api/todos', $scope.formData)
+            .success(function(data) {
+                $scope.formData = {};
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+
+    $scope.deleteTodo = function(id) {
+        $http.delete('/api/todos/' + id)
+            .success(function(data) {
+                $scope.todos = data;
+                console.log(data);
+            })
+            .error(function(data) {
+                console.log('Error: ' + data);
+            });
+    };
+});
---
+++ server.js
@@ -0,0 +1,62 @@
+var express  = require('express');
+var app      = express();
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override'); // simulate DELETE and PUT (express4)
+
+mongoose.connect('PUT URL OF MONGO DB HERE');
+
+app.use(express.static(__dirname + '/public'));                
+app.use(bodyParser.urlencoded({'extended':'true'}));            
+app.use(bodyParser.json());                                     
+app.use(bodyParser.json({ type: 'application/vnd.api+json' })); 
+app.use(methodOverride());
+
+var Todo = mongoose.model('Todo', {
+    text : String
+});
+
+
+app.get('/api/todos', function(req, res) {
+    Todo.find(function(err, todos) {
+        if (err)
+            res.send(err)
+
+        res.json(todos);
+    });
+});
+
+app.post('/api/todos', function(req, res) {
+    Todo.create({
+        text : req.body.text,
+        done : false
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
+
+app.delete('/api/todos/:todo_id', function(req, res) {
+    Todo.remove({
+        _id : req.params.todo_id
+    }, function(err, todo) {
+        if (err)
+            res.send(err);
+
+        Todo.find(function(err, todos) {
+            if (err)
+                res.send(err)
+            res.json(todos);
+        });
+    });
+});
+
+
+app.listen(9000, "Listening at port 9000.");
